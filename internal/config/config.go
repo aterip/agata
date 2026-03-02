945fa77888a582d28204f40be40ce2050e9d62a3
aterip <a.malakhov@gmail.com>
2026-03-02 13:16:04 +0300

    Add router, test handlers and start srvr

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,66 @@
 package config
 
-type serverBasicConfig struct {
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type ServerConfig struct {
+	IsDebug *bool
+	Listen  struct {
+		Protocol string `yaml:"protocol" env:"PROTOCOL"`
+		BindIP   string `yaml:"host" env:"HOST"`
+		Port     string `yaml:"port" env:"PORT"`
+	}
 }
 
-type clickhouseBasicConfig struct {
-	username     string
-	pwd          string
-	dataBaseName string
+type DataBasesList struct {
+	DataBase map[string]bool `yaml:"dbList" env:"DBLIST"`
 }
 
-type postgresBasicConfig struct {
-	username     string
-	pwd          string
-	dataBaseName string
+type PostgresBasicConfig struct {
+	Host   string `yaml:"host" env:"HOST_PG"`
+	Port   string `yaml:"port" env:"PORT_PG"`
+	NameDB string `yaml:"namedb" env:"NAMEDB_PG"`
+	User   string `yaml:"usr" env:"USRDB_PG"`
+	Pswd   string `yaml:"pswd" env:"PSWD_PG"`
 }
 
-func uploadConfig() {
+var databasesList *DataBasesList
+var postgresInstance *PostgresBasicConfig
+var instance *ServerConfig
+var once sync.Once
 
+func GetServerConfig() *ServerConfig {
+
+	once.Do(func() {
+		instance = &ServerConfig{}
+		if err := cleanenv.ReadConfig("srvconf.yml", instance); err != nil {
+			help, _ := cleanenv.GetDescription(instance, nil)
+			log.Fatal(help)
+		}
+	})
+	return instance
+}
+
+func GetDataBasesList() *DataBasesList {
+	once.Do(func() {
+		if err := cleanenv.ReadConfig("srvconf.yml", databasesList); err != nil {
+			help, _ := cleanenv.GetDescription(databasesList, nil)
+			log.Fatal(help)
+		}
+	})
+	return databasesList
+}
+
+func GetPostgresConfig() *PostgresBasicConfig {
+	once.Do(func() {
+		if err := cleanenv.ReadConfig("postgres.yml", postgresInstance); err != nil {
+			help, _ := cleanenv.GetDescription(postgresInstance, nil)
+			log.Fatal(help)
+		}
+	})
+	return postgresInstance
 }
--- internal/server/handlers/users.go
+++ internal/server/handlers/users.go
@@ -1,17 +1,30 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func AuthUserHandler(w *http.ResponseWriter, r *http.Response) {
-
+func AuthUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Authentification")
 }
 
-func RegistrationUserHandler(w *http.ResponseWriter, r *http.Response) {
-
+func RegistrationUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Registration")
 }
 
-func EditUserHandler(w *http.ResponseWriter, r *http.Response) {
+func EditUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Edit")
+}
 
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Delete")
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get user")
+}
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get users")
 }
